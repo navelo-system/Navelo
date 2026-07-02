26c7062cc7f4e14deaf2d8278cf30deb64173724
WitherZx <marcos.contatoprof@gmail.com>
2026-07-02 06:39:55 -0300

    Novo Commit

--- Navelo/src/components/store/advanced/Header.tsx
+++ Navelo/src/components/store/advanced/Header.tsx
@@ -9,6 +9,7 @@ import { Input } from "../base/Input"
 import { Grid } from "../base/Grid"
 import { Button } from "../base/Button"
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "../base/Modal"
+import { CircularIcon } from "../intermediary/CircularIcon"
 import { Eye, Cloud, LogOut, Settings, Upload } from "lucide-react"
 
 const isValidHex = (color: string | null): color is string => {
@@ -117,9 +118,12 @@ export const Header: React.FC = () => {
       {/* Settings Modal */}
       <Modal isOpen={isSettingsOpen} onClose={() => setIsSettingsOpen(false)} hideCloseButton={true}>
         <ModalHeader>
-          <Stack gap={1}>
-            <Font variant="body-semibold" text="Configurações do Sistema" />
-            <Font variant="description" text="Gerencie suas preferências e recursos do terminal." />
+          <Stack direction="row" align="center" gap={5}>
+            <CircularIcon icon={Settings} size={24} />
+            <Stack gap={1}>
+              <Font variant="body-semibold" text="Configurações do Sistema" />
+              <Font variant="description" text="Gerencie suas preferências e recursos do terminal." />
+            </Stack>
           </Stack>
         </ModalHeader>
         <ModalBody>
